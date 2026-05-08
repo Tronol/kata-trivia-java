ec6487622ff3b5cee05e10421bc12fd6cc12bb6b
Tronol <edgarsalcedoelias@gmail.com>
2026-05-08 09:48:53 -0600

    refactor J1: renombrar identificadores engañosos (places, purses, is_playable)

--- python/game.py
+++ python/game.py
@@ -4,8 +4,8 @@ from collections import deque
 class Game:
     def __init__(self):
         self.players = []
-        self.places = [0] * 6
-        self.purses = [0] * 6
+        self._player_positions = [0] * 6
+        self._player_coins = [0] * 6
         self.in_penalty_box = [False] * 6
 
         self.pop_questions = deque()
@@ -25,13 +25,13 @@ class Game:
     def create_rock_question(self, index):
         return "Rock Question " + str(index)
 
-    def is_playable(self):
+    def has_enough_players(self):
         return self.how_many_players() >= 2
 
     def add(self, player_name):
         idx = self.how_many_players()
-        self.places[idx] = 1
-        self.purses[idx] = 0
+        self._player_positions[idx] = 1
+        self._player_coins[idx] = 0
         self.in_penalty_box[idx] = False
         self.players.append(player_name)
         print(player_name + " was added")
@@ -49,20 +49,20 @@ class Game:
             if roll % 2 != 0:
                 self.is_getting_out_of_penalty_box = True
                 print(self.players[self.current_player] + " is getting out of the penalty box")
-                self.places[self.current_player] = self.places[self.current_player] + roll
-                if self.places[self.current_player] > 12:
-                    self.places[self.current_player] = self.places[self.current_player] - 12
-                print(self.players[self.current_player] + "'s new location is " + str(self.places[self.current_player]))
+                self._player_positions[self.current_player] = self._player_positions[self.current_player] + roll
+                if self._player_positions[self.current_player] > 12:
+                    self._player_positions[self.current_player] = self._player_positions[self.current_player] - 12
+                print(self.players[self.current_player] + "'s new location is " + str(self._player_positions[self.current_player]))
                 print("The category is " + self._current_category())
                 self._ask_question()
             else:
                 print(self.players[self.current_player] + " is not getting out of the penalty box")
                 self.is_getting_out_of_penalty_box = False
         else:
-            self.places[self.current_player] = self.places[self.current_player] + roll
-            if self.places[self.current_player] > 12:
-                self.places[self.current_player] = self.places[self.current_player] - 12
-            print(self.players[self.current_player] + "'s new location is " + str(self.places[self.current_player]))
+            self._player_positions[self.current_player] = self._player_positions[self.current_player] + roll
+            if self._player_positions[self.current_player] > 12:
+                self._player_positions[self.current_player] = self._player_positions[self.current_player] - 12
+            print(self.players[self.current_player] + "'s new location is " + str(self._player_positions[self.current_player]))
             print("The category is " + self._current_category())
             self._ask_question()
 
@@ -77,23 +77,23 @@ class Game:
             print(self.rock_questions.popleft())
 
     def _current_category(self):
-        if self.places[self.current_player] - 1 == 0: return "Pop"
-        if self.places[self.current_player] - 1 == 4: return "Pop"
-        if self.places[self.current_player] - 1 == 8: return "Pop"
-        if self.places[self.current_player] - 1 == 1: return "Science"
-        if self.places[self.current_player] - 1 == 5: return "Science"
-        if self.places[self.current_player] - 1 == 9: return "Science"
-        if self.places[self.current_player] - 1 == 2: return "Sports"
-        if self.places[self.current_player] - 1 == 6: return "Sports"
-        if self.places[self.current_player] - 1 == 10: return "Sports"
+        if self._player_positions[self.current_player] - 1 == 0: return "Pop"
+        if self._player_positions[self.current_player] - 1 == 4: return "Pop"
+        if self._player_positions[self.current_player] - 1 == 8: return "Pop"
+        if self._player_positions[self.current_player] - 1 == 1: return "Science"
+        if self._player_positions[self.current_player] - 1 == 5: return "Science"
+        if self._player_positions[self.current_player] - 1 == 9: return "Science"
+        if self._player_positions[self.current_player] - 1 == 2: return "Sports"
+        if self._player_positions[self.current_player] - 1 == 6: return "Sports"
+        if self._player_positions[self.current_player] - 1 == 10: return "Sports"
         return "Rock"
 
     def handle_correct_answer(self):
         if self.in_penalty_box[self.current_player]:
             if self.is_getting_out_of_penalty_box:
                 print("Answer was correct!!!!")
-                self.purses[self.current_player] += 1
-                print(self.players[self.current_player] + " now has " + str(self.purses[self.current_player]) + " Gold Coins.")
+                self._player_coins[self.current_player] += 1
+                print(self.players[self.current_player] + " now has " + str(self._player_coins[self.current_player]) + " Gold Coins.")
                 winner = self._did_player_win()
                 self.current_player += 1
                 if self.current_player == len(self.players):
@@ -106,8 +106,8 @@ class Game:
                 return True
         else:
             print("Answer was corrent!!!!")
-            self.purses[self.current_player] += 1
-            print(self.players[self.current_player] + " now has " + str(self.purses[self.current_player]) + " Gold Coins.")
+            self._player_coins[self.current_player] += 1
+            print(self.players[self.current_player] + " now has " + str(self._player_coins[self.current_player]) + " Gold Coins.")
             winner = self._did_player_win()
             self.current_player += 1
             if self.current_player == len(self.players):
@@ -124,4 +124,4 @@ class Game:
         return True
 
     def _did_player_win(self):
-        return not (self.purses[self.current_player] == 6)
+        return not (self._player_coins[self.current_player] == 6)
